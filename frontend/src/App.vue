c62c4b0f5def328105e1c3e70b2aec7b8f3a820e
EduNeck <82963447+EduNeck@users.noreply.github.com>
2025-05-08 19:23:50 -0500

    Proyecto Base

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="text-center">
+        <h1>¡Proyecto SIGCAL funcionando con Vuetify!</h1>
+        <v-btn color="primary" @click="saludar">Haz clic aquí</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  methods: {
+    saludar() {
+      alert('¡Vuetify está funcionando correctamente!')
+    }
+  }
+}
+</script>
