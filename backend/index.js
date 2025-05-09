736668e84632fbd81097cbc1403d0bfb7ddca23a
EduNeck <82963447+EduNeck@users.noreply.github.com>
2025-05-08 19:44:57 -0500

    ajuste backend

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const cors = require('cors')
+const pool = require('./db')
+require('dotenv').config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('API funcionando correctamente.')
+  res.send('API SIGCAL funcionando')
+})
+
+app.get('/api/ping', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()')
+    res.json(result.rows[0])
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Error al conectar con PostgreSQL')
+  }
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Servidor backend corriendo en http://localhost:${PORT}`)
+  console.log(`Backend corriendo en http://localhost:${PORT}`)
 })
