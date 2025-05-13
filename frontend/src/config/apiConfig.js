e774a69197be11d932aca94b592d0232205225ed
EduNeck <82963447+EduNeck@users.noreply.github.com>
2025-05-12 21:44:16 -0500

    Ajustes Finales de variables de conexion de doker

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,10 +1,8 @@
 // src/config/apiConfig.js
-const API_BASE_URL = '/api';
+const API_BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api`;
 
 if (!API_BASE_URL || API_BASE_URL === 'undefined') {
-  if (import.meta?.env?.MODE !== 'production') {
-    console.warn('[ADVERTENCIA] API_BASE_URL no está correctamente definido en apiConfig.js');
-  }
+  console.warn('[⚠️] API_BASE_URL no está definido correctamente.');
 }
 
 export default API_BASE_URL;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store';
 import vuetify from './plugins/vuetify'
+export const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
 
 const app = createApp(App)
 app.use(router)
