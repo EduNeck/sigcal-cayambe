484273f67ccc1ed780d2be915611c60c8ce1cedf
EduNeck <82963447+EduNeck@users.noreply.github.com>
2025-05-12 19:54:48 -0500

    Ajustes de archivos para Doquerización

---
+++ frontend/src/config/rolesConfig.js
@@ -0,0 +1,14 @@
+export const rolesConfig = {
+  Consulta: {
+    canNavigate: true,
+    canUseButtons: false,
+  },
+  Edicion: {
+    canNavigate: true,
+    canUseButtons: true,
+  },
+  Valoracion: {
+    canNavigate: true,
+    canUseButtons: true,
+  },
+};
