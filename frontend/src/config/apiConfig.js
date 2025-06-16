09b04215eafe6486b21687845a89237aaff9da05
EduNeck <82963447+EduNeck@users.noreply.github.com>
2025-06-16 12:14:14 -0500

    Ajsutes de Puertos

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,6 +1,6 @@
 // src/config/apiConfig.js
-const API_BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api`;
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// Verifica si la variable de entorno está definida
 if (!API_BASE_URL || API_BASE_URL === 'undefined') {
   console.warn('[⚠️] API_BASE_URL no está definido correctamente.');
 }
